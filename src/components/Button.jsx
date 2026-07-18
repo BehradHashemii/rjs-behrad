79ae94b707c3ea04fe86808cb4a9d69006cf3a2d
BehradHashemi <behradhashemi1386@gmail.com>
2026-07-18 17:29:24 +0330

    fix Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Button.module.css";
 
 function Button({ title, icon, link }) {
+  const navigate = useNavigate();
   return (
     <button onClick={() => navigate(`/${link}`)} className={styles.Button}>
       <span>{title}</span>
